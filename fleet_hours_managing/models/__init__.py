3ef1ef3348316da7b53fe442e9dc8badb47bfe90
Moussa <37268998+cameleonRuoi@users.noreply.github.com>
2023-04-30 00:15:24 +0000

    Delete fleet_hours_managing directory

--- fleet_hours_managing/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import fleet_vehicle
-from . import fleet_vehicle_cost
-from . import stock_picking
-from . import res_config_settings
--- fleet_hours_managing/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class fleet_hours_managing(models.Model):
-    _name = 'fleet_hours_managing.fleet_hours_managing'
-    _description = 'Gestion Horaire des véhicules et Engins'
-
-    
