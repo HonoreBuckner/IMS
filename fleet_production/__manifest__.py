56b585db5ea226d1bb0c1992f0cdd2151fb8b2e3
Moussa <37268998+cameleonRuoi@users.noreply.github.com>
2026-01-05 01:08:19 +0000

    Add files via upload

--- fleet_production/__manifest__.py
+++ fleet_production/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Fleet Production',
-    'version': '16.0.1',
-    'summary': 'Record the time of vehicle worked, create a sale order of worked hours',
+    'version': '18.0.1.0',
+    'summary': 'Record the time of vehicle worked, create a sale order of worked hours ok',
     'description': 'record vehicle worked hours daily, ',
     'category': 'Category',
     'author': 'AOS Mali',
@@ -12,3 +12,4 @@
     'installable': True,
     'auto_install': False
 }
+
